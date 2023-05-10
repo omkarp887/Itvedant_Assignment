8b1af11b5274dd351d75b23da1e791525ac917e1
Omkar Pradip Patil <88555432+omkarp887@users.noreply.github.com>
2023-05-10 17:52:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Select the video element
+
+// const video = document.querySelector("video");
+
+// // Pause the video when it ends
+
+// video.addEventListener("ended", function () {
+//   this.pause();
+// });
+
+// Add a class to the current version of HTML in the list
+
+// const versions = document.querySelectorAll("li");
+
+// versions.forEach(function (version) {
+//   if (version.textContent.includes("HTML5")) {
+//     version.classList.add("current");
+//   }
+// });
